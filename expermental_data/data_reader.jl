6f6d071990a6ae1f521f631bc1739e2940de2823
Nirbhay0007 <109225815+Nirbhay0007@users.noreply.github.com>
2025-12-15 10:47:32 +0530

    some updates

--- expermental_data/data_reader.jl
+++ expermental_data/data_reader.jl
@@ -1,10 +1,12 @@
 using DataFrames
 using CSV
 using Plots
+using JLD
 
-df = CSV.read("C:/Users/Admin/Downloads/Neural_Spiking_Dynamics/expermental_data/neuron_data_noise1.csv", DataFrame)
+df = CSV.read("expermental_data/neuron_data_noise1.csv", DataFrame)
 
 
 
 # WE are going to find the first spike V above -20mV
 spike_index = findfirst(df.voltage_mv.> -20)
+plot(df.time_ms,df.voltage_mv)
